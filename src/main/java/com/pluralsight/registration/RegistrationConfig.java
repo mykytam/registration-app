911a27ad2959b9a213241190266294e10ba4d02e
mykytam <morar.nikita@gmail.com>
2020-04-15 21:26:03 +0300

    Added Internationalization

--- src/main/java/com/pluralsight/registration/RegistrationConfig.java
+++ src/main/java/com/pluralsight/registration/RegistrationConfig.java
@@ -1,20 +1,51 @@
 package com.pluralsight.registration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class RegistrationConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("WEB-INF/pdf/");
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
 
     @Bean
     public ViewResolver viewResolver() {
@@ -24,5 +55,4 @@ public class RegistrationConfig implements WebMvcConfigurer {
         bean.setOrder(0);
         return bean;
     }
-
 }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -1,5 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
 <html>
 <head>
     <title>Registration</title>
@@ -10,7 +12,7 @@
     <table>
         <tr>
             <td>
-                Name:
+                <spring:message code="name"/>:
             </td>
             <td>
                 <form:input path="name"/>
@@ -18,7 +20,7 @@
         </tr>
         <tr>
             <td colspan="2">
-                <input type="submit" value="Add Registration">
+                <input type="submit" value="Save">
             </td>
         </tr>
     </table>
