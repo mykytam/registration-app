3626af514edf9d7937a9366f69b4ef4d32a97536
mykytam <morar.nikita@gmail.com>
2020-04-16 17:33:12 +0300

    Validating Objects

--- src/main/java/com/pluralsight/registration/model/Registration.java
+++ src/main/java/com/pluralsight/registration/model/Registration.java
@@ -2,7 +2,11 @@ package com.pluralsight.registration.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class Registration {
+
+    @NotEmpty
     private String name;
 }
